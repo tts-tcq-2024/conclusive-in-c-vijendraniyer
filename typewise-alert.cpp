e15c3610b82c5f9342bb73919b7f699672dd3227
Vijendran Iyer <vijendraniyer@gmail.com>
2024-11-02 12:53:53 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -3,87 +3,27 @@
 #include <stdexcept>
 
 // Define limits for different cooling types
-double getPassiveCoolingLowerLimit() {
-    return 0; // Passive cooling lower limit
+CoolingLimits getPassiveCoolingLimits() {
+    return {0, 35}; // Passive cooling limits
 }
 
-double getPassiveCoolingUpperLimit() {
-    return 35; // Passive cooling upper limit
+CoolingLimits getHiActiveCoolingLimits() {
+    return {0, 45}; // High active cooling limits
 }
 
-double getHiActiveCoolingLowerLimit() {
-    return 0; // High active cooling lower limit
-}
-
-double getHiActiveCoolingUpperLimit() {
-    return 45; // High active cooling upper limit
-}
-
-double getMedActiveCoolingLowerLimit() {
-    return 0; // Medium active cooling lower limit
-}
-
-double getMedActiveCoolingUpperLimit() {
-    return 40; // Medium active cooling upper limit
+CoolingLimits getMedActiveCoolingLimits() {
+    return {0, 40}; // Medium active cooling limits
 }
 
 CoolingLimits getCoolingLimits(CoolingType coolingType) {
-    CoolingLimits limits;
-
     switch (coolingType) {
         case PASSIVE_COOLING:
-            limits.lowerLimit = getPassiveCoolingLowerLimit();
-            limits.upperLimit = getPassiveCoolingUpperLimit();
-            break;
+            return getPassiveCoolingLimits();
         case HI_ACTIVE_COOLING:
-            limits.lowerLimit = getHiActiveCoolingLowerLimit();
-            limits.upperLimit = getHiActiveCoolingUpperLimit();
-            break;
+            return getHiActiveCoolingLimits();
         case MED_ACTIVE_COOLING:
-            limits.lowerLimit = getMedActiveCoolingLowerLimit();
-            limits.upperLimit = getMedActiveCoolingUpperLimit();
-            break;
+            return getMedActiveCoolingLimits();
         default:
             throw std::invalid_argument("Invalid cooling type");
-    }
-
-    return limits;
-}
-
-// Updated function to return BreachType
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperature) {
-    CoolingLimits limits = getCoolingLimits(coolingType);
-    if (temperature < limits.lowerLimit) {
-        return TOO_LOW; // Too low
-    } else if (temperature > limits.upperLimit) {
-        return TOO_HIGH; // Too high
-    }
-    return NORMAL; // Normal
-}
-
-void sendToController(BreachType breachType) {
-    // Implement the logic for sending alerts to the controller
-    if (breachType == TOO_LOW) {
-        std::cout << "Sending alert: Temperature too low!" << std::endl;
-    } else if (breachType == TOO_HIGH) {
-        std::cout << "Sending alert: Temperature too high!" << std::endl;
-    }
-}
-
-void sendToEmail(BreachType breachType) {
-    // Implement the logic for sending alerts via email
-    if (breachType == TOO_LOW) {
-        std::cout << "Email alert: Temperature too low!" << std::endl;
-    } else if (breachType == TOO_HIGH) {
-        std::cout << "Email alert: Temperature too high!" << std::endl;
-    }
-}
-
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryCharacter, double temperature) {
-    BreachType breachType = classifyTemperatureBreach(batteryCharacter.coolingType, temperature);
-    if (alertTarget == TO_CONTROLLER) {
-        sendToController(breachType);
-    } else if (alertTarget == TO_EMAIL) {
-        sendToEmail(breachType);
     }
 }
