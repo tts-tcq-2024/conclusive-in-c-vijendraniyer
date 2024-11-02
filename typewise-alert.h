df3cc228585e6468521b63415b011d0e84efb108
Vijendran Iyer <vijendraniyer@gmail.com>
2024-11-02 13:21:13 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,24 +1,30 @@
 #ifndef TYPEWISE_ALERT_H
 #define TYPEWISE_ALERT_H
 
+/**
+ * @brief Enum representing different cooling types.
+ */
 enum CoolingType {
-    PASSIVE_COOLING,
-    HI_ACTIVE_COOLING,
-    MED_ACTIVE_COOLING
+    PASSIVE_COOLING,  ///< Passive cooling type
+    HI_ACTIVE_COOLING, ///< High active cooling type
+    MED_ACTIVE_COOLING ///< Medium active cooling type
 };
 
+/**
+ * @brief Structure to hold cooling limits.
+ */
 struct CoolingLimits {
-    double lower;
-    double upper;
+    double lower; ///< Lower limit of cooling
+    double upper; ///< Upper limit of cooling
 };
 
-// Original function declaration
+// Function declarations for retrieving cooling limits
 CoolingLimits getCoolingLimits_Passive(CoolingType coolingType_Passive);
 CoolingLimits getCoolingLimits_HiActive(CoolingType coolingType_HiActive);
 CoolingLimits getCoolingLimits_MedActive(CoolingType coolingType_MedActive);
 CoolingLimits getCoolingLimits_Invalid(CoolingType coolingType_Invalid);
 
-// New helper function declarations
+// New helper function declarations for specific cooling limits
 CoolingLimits getPassiveCoolingLimits();
 CoolingLimits getHiActiveCoolingLimits();
 CoolingLimits getMedActiveCoolingLimits();
