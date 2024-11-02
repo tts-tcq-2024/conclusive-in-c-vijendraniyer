17cad5a960fed345abafee42438c463e4ac18e81
Vijendran Iyer <vijendraniyer@gmail.com>
2024-11-02 12:50:28 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,41 +1,23 @@
+// typewise-alert.h
+
 #ifndef TYPEWISE_ALERT_H
 #define TYPEWISE_ALERT_H
 
-typedef enum {
-    PASSIVE_COOLING,
-    HI_ACTIVE_COOLING,
-    MED_ACTIVE_COOLING
-} CoolingType;
+enum CoolingType { PASSIVE_COOLING, HI_ACTIVE_COOLING, MED_ACTIVE_COOLING };
+enum BreachType { NORMAL, TOO_LOW, TOO_HIGH };
+enum AlertTarget { TO_CONTROLLER, TO_EMAIL };
 
-typedef struct {
+struct CoolingLimits {
     double lowerLimit;
     double upperLimit;
-} CoolingLimits;
+};
 
-typedef enum {
-    TOO_LOW,
-    NORMAL,
-    TOO_HIGH
-} BreachType;
-
-typedef enum {
-    TO_CONTROLLER,
-    TO_EMAIL
-} AlertTarget;
-
-typedef struct {
+struct BatteryCharacter {
     CoolingType coolingType;
-    // Other battery characteristics can be added here
-} BatteryCharacter;
+};
 
 CoolingLimits getCoolingLimits(CoolingType coolingType);
-double getPassiveCoolingLowerLimit();
-double getPassiveCoolingUpperLimit();
-double getHiActiveCoolingLowerLimit();
-double getHiActiveCoolingUpperLimit();
-double getMedActiveCoolingLowerLimit();
-double getMedActiveCoolingUpperLimit();
-double classifyTemperatureBreach(CoolingType coolingType, double temperature);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperature); // Change return type to BreachType
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryCharacter, double temperature);
