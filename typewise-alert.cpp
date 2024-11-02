bb3d8e3fc583ece6364235c7f268a26c9cdf5b9a
Vijendran Iyer <vijendraniyer@gmail.com>
2024-11-02 12:33:58 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -11,20 +11,46 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
     return NORMAL;
 }
 
+// Helper function to define lower limit based on cooling type
+int getPassiveCoolingLowerLimit() {
+    return 0;
+}
+
+int getHiActiveCoolingLowerLimit() {
+    return 0;
+}
+
+int getMedActiveCoolingLowerLimit() {
+    return 0;
+}
+
 int getLowerLimit(CoolingType coolingType) {
     switch (coolingType) {
-        case PASSIVE_COOLING: return 0;
-        case HI_ACTIVE_COOLING: return 0;
-        case MED_ACTIVE_COOLING: return 0;
+        case PASSIVE_COOLING: return getPassiveCoolingLowerLimit();
+        case HI_ACTIVE_COOLING: return getHiActiveCoolingLowerLimit();
+        case MED_ACTIVE_COOLING: return getMedActiveCoolingLowerLimit();
         default: return -1; // Error case, should not occur
     }
 }
 
+// Helper function to define upper limit based on cooling type
+int getPassiveCoolingUpperLimit() {
+    return 35;
+}
+
+int getHiActiveCoolingUpperLimit() {
+    return 45;
+}
+
+int getMedActiveCoolingUpperLimit() {
+    return 40;
+}
+
 int getUpperLimit(CoolingType coolingType) {
     switch (coolingType) {
-        case PASSIVE_COOLING: return 35;
-        case HI_ACTIVE_COOLING: return 45;
-        case MED_ACTIVE_COOLING: return 40;
+        case PASSIVE_COOLING: return getPassiveCoolingUpperLimit();
+        case HI_ACTIVE_COOLING: return getHiActiveCoolingUpperLimit();
+        case MED_ACTIVE_COOLING: return getMedActiveCoolingUpperLimit();
         default: return -1; // Error case, should not occur
     }
 }
@@ -54,16 +80,11 @@ void sendToController(BreachType breachType) {
 
 void sendToEmail(BreachType breachType) {
     const char* recipient = "a.b@c.com";
-    switch (breachType) {
-        case TOO_LOW:
-            printf("To: %s\n", recipient);
-            printf("Hi, the temperature is too low\n");
-            break;
-        case TOO_HIGH:
-            printf("To: %s\n", recipient);
-            printf("Hi, the temperature is too high\n");
-            break;
-        case NORMAL:
-            break;
+    if (breachType == TOO_LOW) {
+        printf("To: %s\n", recipient);
+        printf("Hi, the temperature is too low\n");
+    } else if (breachType == TOO_HIGH) {
+        printf("To: %s\n", recipient);
+        printf("Hi, the temperature is too high\n");
     }
 }
